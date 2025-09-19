bf113aea962f8bf8f5dab5a96556a7cddca88857
Mazen Al Swar <43959520+mazenswar@users.noreply.github.com>
2025-09-19 12:38:14 -0400

    v1.7

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./components/Navbar/Component";
 import Footer from "./components/Footer/Component";
 import "./styles/main.scss";
 import { Tajawal } from "next/font/google";
+import Script from "next/script";
 
 const tajawal = Tajawal({
 	subsets: ["arabic"],
@@ -26,6 +26,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				{/* Google Analytics */}
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=G-033B723TEJ`}
+					strategy="afterInteractive"
+				/>
+				<Script id="ga-init" strategy="afterInteractive">
+					{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-033B723TEJ');
+          `}
+				</Script>
+			</head>
 			<body className={`${tajawal.variable}`}>
 				<Navbar />
 				{children}
